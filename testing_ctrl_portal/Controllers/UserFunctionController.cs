24fe9c5dabbc6cdecc1e04101a090f0249efc51e
Kristen Harrell <71024265+kristen-harrell@users.noreply.github.com>
2021-02-04 13:53:07 -0500

    validate code action added

--- testing_ctrl_portal/Controllers/UserFunctionController.cs
+++ testing_ctrl_portal/Controllers/UserFunctionController.cs
@@ -58,6 +58,71 @@ namespace testing_ctrl_portal.Controllers
 
             return View("index");
         }
+        [HttpPost]
+        public IActionResult SaveCode(string email)
+        {
 
+            PersistCode entry = new PersistCode();
+            entry.Email = email;
+            entry.ResetCode = GetCode();
+            entry.Expiration = DateTime.Now.AddMinutes(10);
+
+
+            _db.PersistCode.Add(entry);
+            _db.SaveChanges();
+
+            ViewBag.Confirmation = $"your reset code has been sent to {email}";
+
+            return View("index");
+        }
+        public static string GetCode()
+        {
+            int stringLength = 6;
+            var stringBuilder = new StringBuilder();
+
+            for (int i = 0; i < stringLength; i++)
+            {
+                Random random = new Random();
+                string num = random.Next(0, 10).ToString();
+                char alpha = (char)random.Next('a', 'z'); //if I make the set 'A', 'z' to try to incl. caps and lower, I get special char too
+                if (random.Next() % 2 == 0)
+                {
+                    stringBuilder.Append(num);
+                }
+                else
+                {
+                    stringBuilder.Append(alpha);
+                }
+            }
+           string code = stringBuilder.ToString();
+
+            return code;
+        }
+        [HttpPost]
+        public IActionResult ValidateCode(string code)
+        {
+            string response = "";
+
+            List<PersistCode> validCode = _db.PersistCode.Where(x => x.ResetCode == code)
+                                           .Where(x => x.Expiration > DateTime.Now).ToList();
+
+            if (validCode.Count == 1)
+            {
+                response = "this code is valid for resetting your password";
+            }    
+
+            if (validCode.Count < 1)
+            {
+                response = "this code is no longer valid.  Please request a new one";
+
+            }
+
+
+            ViewBag.CodeResponse = $"{response}";
+
+
+           
+            return View("index");
+        }
     }
 }
